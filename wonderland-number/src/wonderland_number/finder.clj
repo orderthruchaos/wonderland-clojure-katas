583a2e98a38bd5ecbec3d194aaaf4700c1522cd3
Brett N. DiFrischia <orderthruchaos@gmail.com>
2015-08-04 10:40:10 -0500

    Adding solution to wonderland-number.

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,23 @@
 (ns wonderland-number.finder)
 
+
+(def min-number (reduce * (repeat 5 10)))
+(def max-number (* 10 min-number))
+
+(defn number-to-digits [n] (set (map int (str n))))
+(defn numbers-have-same-digits? [n1 n2]
+  (= (number-to-digits n1) (number-to-digits n2)))
+
+(def multipliers #{2 3 4 5 6})
+
+(defn try-multipliers [n]
+  (= multipliers
+     (set (filter #(numbers-have-same-digits? n (* n %)) multipliers))))
+
+(defn wonderland-condition [n] (if (try-multipliers n) n false))
+
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (some wonderland-condition (range min-number max-number)))
+
+; (use 'wonderland-number.finder)
+; (wonderland-number)
