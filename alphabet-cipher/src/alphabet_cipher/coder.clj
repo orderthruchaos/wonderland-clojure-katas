643e067f3f9b21bd58d2569883d0165bdf096bd9
Brett N. DiFrischia <orderthruchaos@gmail.com>
2015-08-04 10:36:29 -0500

    Adding solution to alphabet-cipher kata.

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,7 +1,55 @@
 (ns alphabet-cipher.coder)
 
-(defn encode [keyword message]
-  "encodeme")
+(defn- rot-by [n c]
+  (let [v   (into [] c)
+        len (count v)
+        nr  (mod n len)]
+    (concat (drop nr v) (take nr v))))
 
-(defn decode [keyword message]
-  "decodeme")
+(def all-caps (map char (range (int \A) (inc (int \Z)))))
+
+(defn- shift-for [l] (- (int l) (int \A)))
+
+(defn-
+  rot-map [n c]
+  (if (char? n)
+    (recur (shift-for n) c)
+    (into {} (map (fn [v1 v2] [v1 v2]) c (rot-by n c)))))
+
+(def encode-map (into {} (map (fn [l] [l (rot-map l all-caps)]) all-caps)))
+
+(def decode-map
+  (into {}
+        (map (fn [[k v]]
+               [k 
+                (into {} (for [[k1 v1] v] [v1 k1] ))
+                ]) encode-map)))
+
+(defn-
+  phrase-to-seq [s]
+  (->> s
+       (clojure.string/upper-case)
+       (filter #(<= (int \A) (int %) (int \Z)))))
+
+(defn- cypher-with-map
+  [msg, kwd, m]
+  (let [msgs (phrase-to-seq msg)
+        rptc (inc (quot (count msg) (count kwd)))
+        kwds (take
+               (count msgs)
+               (phrase-to-seq (apply str (repeat rptc kwd))))]
+    (clojure.string/lower-case
+      (apply str (map #(get (get m %2) %1) msgs kwds)))))
+
+(def encode (fn [kwd msg] (cypher-with-map msg kwd encode-map)))
+; (def
+;   encode (fn [msg, kwd]
+;            (let [msgs (phrase-to-seq msg)
+;                  rptc (inc (quot (count msg) (count kwd)))
+;                  kwds (take
+;                         (count msgs)
+;                         (phrase-to-seq (apply str (repeat rptc kwd))))]
+;              (apply str (map #(get (get encode-map %2) %1) msgs kwds))))
+;   )
+
+(def decode (fn [kwd msg] (cypher-with-map msg kwd decode-map)))
