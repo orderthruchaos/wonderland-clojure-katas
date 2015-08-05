ecab3c79ee484f30c166a57a6e452e596198730c
Brett DiFrischia <orderthruchaos@gmail.com>
2015-08-05 14:50:23 -0500

    Adding solution to doublets.  Needs cleaned up.

--- doublets/src/doublets/solver.clj
+++ doublets/src/doublets/solver.clj
@@ -1,15 +1,32 @@
 (ns doublets.solver
   (:require [clojure.java.io :as io]
             [clojure.edn :as edn]
+            [clojure.set :refer :all]
+            [clojure.pprint :refer [pprint]]
             [clojure.string]))
 
-; (load-file "src/doublets/solver.clj")
-; (use 'doublets.solver)
+; (load-file "src/doublets/solver.clj") (use 'doublets.solver)
 
-(def words (-> "words.edn"
-               (io/resource)
-               (slurp)
-               (read-string)))
+(def ^:private words (-> "words.edn"
+                         (io/resource)
+                         (slurp)
+                         (read-string)))
+
+(defn words-of-correct-length [w]
+  (let [l (count w)]
+    (sort (filter #(= l (count %)) words))))
+
+(defn empty-node [t] {:word   nil
+                      :found  []
+                      :regex  nil
+                      :target t
+                      :list   (words-of-correct-length t)})
+
+(def target (atom {:word nil
+                   :list []}))
+
+(defn solver-branch? [n]
+  (not (and (map? n) (= (:word @target) (get n :word)))))
 
 (defn word-to-regex-part [w i _]
   (apply str (concat (take i w) "." (drop (inc i) w))))
@@ -23,11 +40,41 @@
          ")$"))
   )
 
-(defn words-of-correct-length [w]
-  (let [l (count w)]
-    (sort (filter #(= l (count %)) words))))
+(defn word-to-node [w m]
+  (-> m
+      (assoc :word w)
+      (assoc :found (conj (get m :found []) w))
+      (assoc :regex (word-to-regex w))
+      )
+  )
 
-(def print-words #(println words))
+(defn solver-children [n]
+  (let [re (get n :regex)
+        ws (set (filter #(re-matches re %) (:list @target)))]
+    (map #(word-to-node % n) (sort (difference ws (set (get n :found [])))))))
+
+(defn solver-tree [w1 w2] (tree-seq solver-branch?
+                                    solver-children
+                                    (word-to-node w1 (empty-node w2))))
+
+(defn- setup-target! [word2]
+  (swap! target
+         #(-> %
+              (assoc :word word2)
+              (assoc :list (apply vector (words-of-correct-length word2)))
+              )))
+
+(defn solutions [w1 w2]
+  (let [t (solver-tree w1 w2)]
+    (sort-by count (map :found (filter #(= w2 (last (get % :found))) t)))))
 
 (defn doublets [word1 word2]
-  "make me work")
+  (let [l1 (count word1)
+        l2 (count word2)]
+    (setup-target! word2)
+    (if (= l1 l2)
+      (first (solutions word1 word2))
+      [])
+    )
+  )
+
